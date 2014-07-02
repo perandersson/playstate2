07c545f421f634bf086a0118d0f1046690309e25
Per Andersson <dim.raven@gmail.com>
2014-07-02 23:15:59 +0200

    Removed unused function

--- core/rendering/VertexType.cpp
+++ core/rendering/VertexType.cpp
@@ -2,15 +2,6 @@
 #include "VertexType.h"
 using namespace core;
 
-namespace {
-	bool IsIntegerType(const VertexElementDesc& elemDesc)
-	{
-		const VertexElementType::Enum type = elemDesc.type;
-		return type == VertexElementType::INT || type == VertexElementType::UNSIGNED_INT || type == VertexElementType::BYTE
-			|| type == VertexElementType::UNSIGNED_BYTE || type == VertexElementType::SHORT || type == VertexElementType::UNSIGNED_SHORT;
-	}
-}
-
 GLenum VertexElementType::Parse(Enum e)
 {
 	static const GLenum enums[VertexElementType::SIZE] = {
