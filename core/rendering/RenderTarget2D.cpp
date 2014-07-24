04ede02da737369b4cf687d6930f2ce825914072
Per Andersson <dim.raven@gmail.com>
2014-07-24 12:48:42 +0200

    Added lock and unlock method for render targets

--- core/rendering/RenderTarget2D.cpp
+++ core/rendering/RenderTarget2D.cpp
@@ -12,3 +12,13 @@ RenderTarget2D::~RenderTarget2D()
 {
 
 }
+
+void RenderTarget2D::Lock()
+{
+	mMutex.lock();
+}
+
+void RenderTarget2D::Unlock()
+{
+	mMutex.unlock();
+}
--- core/rendering/RenderTarget2D.h
+++ core/rendering/RenderTarget2D.h
@@ -1,5 +1,6 @@
 #pragma once
 #include "Texture2D.h"
+#include <mutex>
 
 namespace core
 {
@@ -8,5 +9,22 @@ namespace core
 	public:
 		RenderTarget2D(GLuint textureID, const Size& size, TextureFormat::Enum format);
 		virtual ~RenderTarget2D();
+
+		/*!
+			\brief Locks this render target.
+
+			This is to ensure that only one thread is rendering to the same RenderTarget at the same time
+		*/
+		void Lock();
+
+		/*!
+			\brief Unlocks this render target
+
+			This is to ensure that only one thread is rendering to the same RenderTarget at the same time
+		*/
+		void Unlock();
+
+	private:
+		std::recursive_mutex mMutex;
 	};
 }
